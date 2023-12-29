d0cb5c78047ebf43e65bf4ca385ef3868f9c2780
DevilVsLife2 <wc3andwarcraft3devilvslive@gmail.com>
2023-12-29 14:18:11 -0700

    change .gitignore

---
+++ dist/lib/index.js
@@ -0,0 +1,47 @@
+var handleId = null;
+export function curl_easy_init() {
+    return;
+}
+;
+function curl_easy_setopt(curl, option, value) {
+    return;
+}
+;
+export function curl_easy_perform(curl, callback) {
+    return;
+}
+;
+export function curl_easy_pause(curl, bitmask) {
+    return;
+}
+;
+export function curl_easy_headers(curl, origin, request) {
+    return;
+}
+;
+export function curl_easy_cleanup(curl) {
+    return;
+}
+;
+export function curl_easy_reset(curl) {
+    return;
+}
+;
+export function curl_easy_getinfo(curl, info) {
+    return;
+}
+;
+export function curl_ws_recv(curl, buffer) {
+    return;
+}
+;
+export function curl_ws_send(curl, buffer, fragsize, flags) {
+    return;
+}
+;
+export function update() {
+    return;
+}
+;
+export var constants = null;
+//# sourceMappingURL=index.js.map
